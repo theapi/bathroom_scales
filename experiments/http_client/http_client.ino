6dff7523bf8986f708b00507796bd4848506ddbd
theapi <petermclarke@gmail.com>
2017-10-04 19:52:33 +0100

    Wifi credentials

--- experiments/http_client/http_client.ino
+++ experiments/http_client/http_client.ino
@@ -1,8 +1,5 @@
 /**
- * BasicHTTPClient.ino
- *
- *  Created on: 24.05.2015
- *
+ * Basic Http client with deep sleep
  */
 
 #include <Arduino.h>
@@ -11,36 +8,30 @@
 #include <WiFiMulti.h>
 #include <HTTPClient.h>
 #include "soc/rtc.h"
-
-#define USE_SERIAL Serial
+#include "credentials.h"
 
 WiFiMulti wifiMulti;
 
 #define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
+#define TIME_TO_SLEEP  60        /* Time ESP32 will go to sleep (in seconds) */
 
 
 void setup() {
     // Slow down the cpu for use with the hx711 library
     rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M);
     
-    USE_SERIAL.begin(115200);
-
-    USE_SERIAL.println();
-    USE_SERIAL.println();
-    USE_SERIAL.println();
+    Serial.begin(115200);
+    Serial.println();
     
-    wifiMulti.addAP("SSID", "PASSWORD");
+    wifiMulti.addAP(WIFI_SID, WIFI_PASSWORD);
 
     // Perform everything needed while awake.
     awake();
 
     // Get ready to sleep
     esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-    Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
-
     // Now sleep
-    Serial.println("Going to sleep now");
+    Serial.print("Awake for: "); Serial.println(millis());
     esp_deep_sleep_start();
 
 }
@@ -53,41 +44,38 @@ void awake() {
 
     esp_sleep_wakeup_cause_t wakeup_reason;
 
-  wakeup_reason = esp_sleep_get_wakeup_cause();
-
-  switch(wakeup_reason) {
-    case 1  : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
-    case 2  : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
-    case 3  : Serial.println("Wakeup caused by timer"); break;
-    case 4  : Serial.println("Wakeup caused by touchpad"); break;
-    case 5  : Serial.println("Wakeup caused by ULP program"); break;
-    default : Serial.println("Wakeup was not caused by deep sleep"); break;
-  }
+    wakeup_reason = esp_sleep_get_wakeup_cause();
+  
+    switch(wakeup_reason) {
+      case 1  : Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+      case 2  : Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+      case 3  : Serial.println("Wakeup caused by timer"); break;
+      case 4  : Serial.println("Wakeup caused by touchpad"); break;
+      case 5  : Serial.println("Wakeup caused by ULP program"); break;
+      default : Serial.println("Wakeup was not caused by deep sleep"); break;
+    }
   
     // wait for WiFi connection
     if((wifiMulti.run() == WL_CONNECTED)) {
 
         HTTPClient http;
-
-        USE_SERIAL.print("[HTTP] begin...\n");
         http.begin("http://192.168.0.22/bathroom_scales.php?w=99"); 
-
-        USE_SERIAL.print("[HTTP] GET...\n");
+        Serial.print("[HTTP] GET...\n");
+        
         // start connection and send HTTP header
         int httpCode = http.GET();
-
         // httpCode will be negative on error
         if (httpCode > 0) {
             // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
 
             // file found at server
             if (httpCode == HTTP_CODE_OK) {
                 String payload = http.getString();
-                USE_SERIAL.println(payload);
+                Serial.println(payload);
             }
         } else {
-            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
         }
 
         http.end();
